b73044a9eba813ee1196c4c416ba277f056fa014
William <willrodrigues4@gmail.com>
2021-06-22 11:13:11 -0300

    Aula 5

---
+++ Modulo_B/Aula_05/ex001.js
@@ -0,0 +1,9 @@
+//Rode os comandos no terminal do Node
+
+var nome = 'William'
+
+nome //Imprime o valor inserido na variavel nome
+
+
+//Verificar o tipo de dados inseridos em uma variavel
+typeof nome //Retornará 'string'

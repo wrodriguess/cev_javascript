3c31e2b8157f31f57cf85f3c7eefcb36b5783dd5
William <willrodrigues4@gmail.com>
2021-06-28 10:26:14 -0300

    Aula 11

---
+++ Modulo_D/Aula_11/ex008.js
@@ -0,0 +1,5 @@
+var velocidade = 60.5
+console.log(`A velocidade do seu carro é ${velocidade}Km/H`)
+if(velocidade > 60)
+    console.log("Você ultrapassou a velocidade máxima permitida. MULTADO")
+console.log("Dirija sempre utilizando cinto de segurança")
---
+++ Modulo_D/Aula_11/ex009.js
@@ -0,0 +1,5 @@
+var pais = 'EUA'
+if(pais != 'Brasil')
+    console.log('Você é Estrangeiro!')
+else
+    console.log('Você é Brasileiro!')

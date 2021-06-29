6c149746c33583f7aeaa516d85c484269d0d93b1
William <willrodrigues4@gmail.com>
2021-06-29 19:35:05 -0300

    Aula 12

---
+++ Modulo_D/Aula_12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 64
+
+if(idade < 16)
+    console.log('Não vota')
+else if(idade >= 16 && idade < 18 || idade >= 65)
+    console.log('Voto Opcional')
+else
+    console.log('Voto obrigatório')
